c811d2cb4381040b398906ec66c3f7cb0ad5e408
bumoh <emmabinie@gmail.com>
2022-12-09 00:12:44 -0800

    Complex puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry
  * Return: Always 0
- */
+ * */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle,\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
