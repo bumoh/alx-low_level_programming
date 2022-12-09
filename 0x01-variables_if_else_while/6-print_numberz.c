f1ee265f5bd8c113dd536a8fd600ad65dbbc20f0
bumoh <emmabinie@gmail.com>
2022-12-09 13:01:43 -0800

    Tasks 1 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints single digits in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+for (num = 0; num <= 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+char le;
+for (le = 'z'; le >= 'a'; le--)
+putchar(le);
+putchar('\n');
+return (0);
+}
