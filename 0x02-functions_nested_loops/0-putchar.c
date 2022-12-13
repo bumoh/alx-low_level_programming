34d0a59ee1ab0b0149faf2c9352a1673d7f3cd8f
bumoh <emmabinie@gmail.com>
2022-12-13 08:55:08 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - Prints _puchar and a newline
+ * main - Prints _putchar and a newline
  * Return: Always 0
  */
-int maun(void)
+int main(void)
 {
 _putchar('_');
 _putchar('p');
