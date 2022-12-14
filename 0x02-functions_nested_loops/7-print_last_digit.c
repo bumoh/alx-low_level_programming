131fe9f81fcad6c13a027e02f4e2511cb704d28f
bumoh <emmabinie@gmail.com>
2022-12-13 19:01:54 -0800

    last digit fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,10 @@
  */
 int print_last_digit(int n)
 {
-int ld = n % 10;
-if (ld < 0)
-ld *= -1;
-_putchar(ld + '0');
-return (0);
+int last;
+last = n % 10;
+if (last < 0)
+last = last * -1;
+_putchar(last + '0');
+return (last);
 }
